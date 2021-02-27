cdde4eb68ca34b6f02ef5b059efa79ede3dc5a80
mypakholchyshyn <64320032+mypakholchyshyn@users.noreply.github.com>
2021-02-27 22:07:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,52 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+const Todo = props => (
+    <li>
+        <input type="checkbox" checked={props.todo.check} onChange={props.onChange} />
+        <button onClick={props.onDelete}>delete</button>
+        <span>{props.todo.text}</span>
+    </li>
+)
+
+let id = 0;
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: []
+        }
+    }
+
+    addTodo() {
+        const text = prompt("add todo");
+        this.setState({ todos: [...this.state.todos, { id: id++, text: text, check: false }] });
+    }
+
+    deleteTodo(id){
+        this.setState({
+            todos: this.state.todos.filter(todo => todo.id !== id)
+        })
+    }
+
+    changeTodo(id){
+        this.setState({ todos: this.state.todos.map(todo => todo.id == id ? { ...todo, check: !todo.check } : todo) })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>My Todo App</h2>
+                <div>Todo count: {this.state.todos.length}</div>
+                <div>Unchecked Todo count: {this.state.todos.filter(todo => !todo.check).length}</div>
+                <button onClick={() => this.addTodo()}>Add todo</button>
+                <ul>
+                    {this.state.todos.map(todo => <Todo 
+                    onDelete ={() => this.deleteTodo(todo.id)} 
+                    onChange ={()=> this.changeTodo(todo.id)} todo={todo} />)}
+                </ul>
+                
+            </div>
+        )
+    }
 }
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-/*let todos = [{id: 991 , text: "task1" , check:true },{id: 992 , text: "task2" , check:true },{id: 993 , text: "task3" , check:true }];
-if(todos.length != 0){
-  render ();
-}
-let id = 0; */
-let todos = [];
-
-let id=todos.length;
-//<li>
-//    <input type="checkbox">
-//    <button>delete</button>
-//    <span>text</span>
-//</li>
-
-class Todo{
-  constructor(){
-    this.id = ++id;
-    this.chek = false;
-    this.text = this.getText();
-  }
-  getText(){
-    return prompt('Enter a todo task:')
-  }
-}
-
-function newTodo() {
-  
-  const todo = new Todo();
-  
-  todos.push(todo);
-  
-  render();
-  
-}
-
-function render(){
-  list.innerHTML = '';
-  todos.map(createTodo).forEach(todo => list.appendChild(todo))
-  // update counts 
-  itemCountSpan.textContent = todos.length;
-  uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
-}
-
-function createTodo(todo){
-  // create li
-  const li = document.createElement('li');
-  
-  // create input checkbox
-  // create button
-  // create span
-  li.innerHTML = `
-  <input type="checkbox" onchange="changeTodo(${todo.id})"${todo.check ? "checked" : ""}>
-  <button onclick = "deleteTodo(${todo.id})">delete</button>
-  <span>${todo.text}</span>
-  `
-  return li;
-  
-
-}
-
-function deleteTodo(id){
-  todos = todos.filter(todo => todo.id !== id);
-  render();
- 
-
-}
-
-function changeTodo(id){
- // for(let i=0; i<todos.length; i++){
- //   if(todos[i].id === id){
- //     todos[i].check = !todos[i].check;
- //     break;
- //   }
- // }
-
- todos = todos.map(todo => todo.id == id ? {id:todo.id, text: todo.text, check:!todo.chek} :todo);
- 
- //todos = todos.map(todo => todo.id == id ? {...todo, check:!todo.chek} :todo);
-  
-
-  uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
-}
+ReactDOM.render(<App />, document.getElementById('root'));
